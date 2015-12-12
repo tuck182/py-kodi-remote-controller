151fc22f2ddc657b0a15e6ba623040ab73830eac
Arn-O <rnd.bertrand@gmail.com>
2015-12-12 15:00:51 +0100

    align outputs

--- pykodi/display/display.py
+++ pykodi/display/display.py
@@ -19,19 +19,19 @@ logger = logging.getLogger(__name__)
 
 def albums_index(albumids, albums):
     """Display albums list from internal index"""
-    logger.debug('call disp_albums_index')
+    logger.debug('call function disp_albums_index')
     print
     for albumid in albumids:
         print "   \"{}\" by {} ({}) [{}]".format(
-                albums[albumid]['title'].encode('UTF-8'),
-                "/".join(albums[albumid]['artist']).encode('UTF-8'),
-                albums[albumid]['year'],
-                albumid
+            albums[albumid]['title'].encode('UTF-8'),
+            "/".join(albums[albumid]['artist']).encode('UTF-8'),
+            albums[albumid]['year'],
+            albumid
         )
 
 def albums_details(albumid, albums):
     """Display album details from albumid"""
-    logger.debug('call albums_details')
+    logger.debug('call function albums_details')
     print
     print "\"{}\" by {} ({})".format(
         albums[albumid]['title'],
@@ -46,68 +46,76 @@ def albums_details(albumid, albums):
 
 def songs_index(songids, songs):
     """Display songs list from internal index"""
-    logger.debug('call songs_index')
+    logger.debug('call function songs_index')
     print
-    for i, songid in enumerate(songids):
-        print ("%02i. \"%s\" by %s (%s) [%i]") % (
-                i + 1,
-                songs[songid]['title'],
-                 "/".join(songs[songid]['artist']),
-                songs[songid]['year'],
-                songid )
+    for songid in songids:
+        print "   \"{}\" by {} ({}) [{}]".format(
+            songs[songid]['title'].encode('UTF-8'),
+            "/".join(songs[songid]['artist']).encode('UTF-8'),
+            songs[songid]['year'],
+            songid
+        )
 
 def songs_details(songid, songs):
     """Display song details from song id"""
-    logger.debug('call songs_details')
+    logger.debug('call function songs_details')
     print
-    print ('"%s" by %s (%s)') % (
-            songs[songid]['title'],
-            "/".join(songs[songid]['artist']),
-            songs[songid]['year'])
-    print "\tDuration: %s" % (
-        str(datetime.timedelta(seconds=songs[songid]['duration'])))
-    print "\tPlaycount: %i (%i)" % (
-            songs[songid]['playcount'],
-            songs[songid]['playcount_en'])
-    print "\tRating: %i (%i)" % (
-            songs[songid]['rating'],
-            songs[songid]['rating_en'])
-    print "\tMusicBrainz ID: %s" % songs[songid]['musicbrainztrackid']
+    print "\"{}\" by {} ({})".format(
+        songs[songid]['title'].encode('UTF-8'),
+        "/".join(songs[songid]['artist']).encode('UTF-8'),
+        songs[songid]['year']
+    )
+    print
+    print "   Duration:        {}".format(
+        str(datetime.timedelta(seconds=songs[songid]['duration']))
+    )
+    print "   Playcount:       {} ({})".format(
+        songs[songid]['playcount'],
+        songs[songid]['playcount_en']
+    )
+    print "   Rating:          {} ({})".format(
+        songs[songid]['rating'],
+        songs[songid]['rating_en']
+    )
+    print "   MusicBrainz ID:  {}".format(
+        songs[songid]['musicbrainztrackid']
+    )
 
 def songs_info(songs):
     """Display song details from song id"""
-    logger.debug('call songs_info')
+    logger.debug('call function songs_info')
     print
-    print "Total number of songs: %i" % len(songs)
+    print "   Total number of songs: %i" % len(songs)
     total_duration = 0
     for songid in songs:
         total_duration += songs[songid]['duration']
     duration_str = str(datetime.timedelta(seconds=total_duration))
-    print "Total duration: %s" % duration_str
+    print "   Total duration: %s" % duration_str
 
 def songs_sync(f_scan, ru_songsids, pcu_songids):
     """Display result of the songs sync process"""
-    logger.debug('call songs_sync')
+    logger.debug('call function songs_sync')
     print
     if f_scan:
-        print "Full scan."
+        print "   A full scan has been performed."
     else:
-        print "Delta scan."
-        print
-        print "\tRatings updated: {}".format(len(ru_songsids))
-        print "\tPlay counts updated: {}".format(len(pcu_songids))
+        print "A delta scan has been performed, {} song(s) with a rating " \
+              "update and {} song(s) with a play count update.".format(
+                len(ru_songsids), len(pcu_songids)
+        )
 
 # playlist
 
 def playlist_show(position, songids, songs):
     """Display playlist"""
-    logger.debug('call playlist_show')
+    logger.debug('call function playlist_show')
     print
     if songids:
         for i, songid in enumerate(songids):
             if i == position:
-                print ">>>",
-            print "\t",
+                print ">> ",
+            else:
+                print "   ",
             print "{}. \"{}\" by {} ({}) [{}]".format(
                 str(i+1).zfill(2),
                 songs[songid]['title'].encode('utf-8'),
@@ -116,10 +124,11 @@ def playlist_show(position, songids, songs):
                 songid
             )
     else:
-        print "\t[playlist empty]"
+        print "   [playlist empty]"
 
 def playlist_now_playing(item, properties):
     """Display the now playing part of play_what"""
+    logger.debug('call function playlist_now_playing')
     print
     if not item:
         print "   [not playing anything]"
@@ -155,6 +164,7 @@ def playlist_now_playing(item, properties):
 
 def playlist_next_playing(items, properties):
     """Display the next playing part of display_what"""
+    logger.debug('call function playlist_show')
     if not properties:
         return
     print
@@ -169,11 +179,16 @@ def playlist_next_playing(items, properties):
 
 def play_album(albumid, albums):
     """Confirm play album"""
-    print "Let's play the album \"%s\" by %s [%i]." % (
-            albums[albumid]['title'], albums[albumid]['artist'], albumid)
+    logger.debug('call function play_album')
+    print "   Let's play the album \"%s\" by %s [%i].".format(
+        albums[albumid]['title'].encode('UTF-8'),
+        "/".join(albums[albumid]['artist']).encode('UTF-8'),
+        albumid
+    )
 
 def play_ban(songid, songs):
     """Confirm ban"""
+    logger.debug('call function play_ban')
     print
     print "   The song \"{}\" by {} [{}] has been banned forever.".format(
         songs[songid]['title'].encode('UTF-8'),
@@ -183,6 +198,7 @@ def play_ban(songid, songs):
 
 def play_favorite(songid, songs):
     """Confirm favorite"""
+    logger.debug('call function play_favorite')
     print
     print "   The song \"{}\" by {} [{}] is now a favorite.".format(
         songs[songid]['title'].encode('UTF-8'),
@@ -192,6 +208,7 @@ def play_favorite(songid, songs):
 
 def play_skip(songid, songs):
     """Confirm skip"""
+    logger.debug('call function play_skip')
     print
     print "   You just have skipped the song \"{}\" by {} [{}].".format(
         songs[songid]['title'].encode('UTF-8'),
@@ -203,56 +220,61 @@ def play_skip(songid, songs):
 
 def en_sync(songids):
     """Display echonest sync results"""
+    logger.debug('call function en_sync')
     if len(songids) == 0:
-        print "Echonest tasteprofile up to date."
+        print "   Echonest tasteprofile up to date."
     else:
         print
-        print "{} song(s) have been updated.".format(len(songids))
+        print "   {} song(s) have been updated.".format(len(songids))
 
 def en_display(song_data):
     """Display echonest song data"""
+    logger.debug('call function en_display')
     print
     print "\"{}\" by {}".format(
         song_data['song_name'].encode('UTF-8'),
         song_data['artist_name'].encode('UTF-8')
     )
     print
-    print "\tEchonest ID: \t\t{}".format(song_data['song_id'])
-    print "\tForeign ID: \t\t{}".format(song_data['foreign_id'])
-    print "\tMusicBrainz ID: \t{}".format(song_data['request']['song_id'])
+    print "   Echonest ID:         {}".format(song_data['song_id'])
+    print "   Foreign ID:          {}".format(song_data['foreign_id'])
+    print "   MusicBrainz ID:      {}".format(song_data['request']['song_id'])
     print
-    print "\tDate added: {} - Last modified: {}".format(
+    print "   Date added: {} - Last modified: {}".format(
         song_data['date_added'], song_data['last_modified'])
     print
-    print "\tThis song has been played {} time(s) and skipped {} time(s)".format(
-        song_data.get('play_count', 0), song_data.get('skip_count', 0))
-    print "\tRating: {} - Favorite: {} - Banned: {}".format(
+    print "   This song has been played {} time(s) and skipped {} time(s)".format(
+        song_data.get('play_count', 0), song_data.get('skip_count', 0)
+    )
+    print "   Rating: {} - Favorite: {} - Banned: {}".format(
         song_data.get('rating', 0),
         song_data.get('favorite', False),
-        song_data.get('banned', False))
+        song_data.get('banned', False)
+    )
     print
-    print "\tSong type(s): {}".format(", ".join(song_data['song_type']))
+    print "   Song type(s):        {}".format(", ".join(song_data['song_type']))
     print
-    print "\tSong currency: \t\t{}".format(song_data['song_currency'])
-    print "\tSong hotttnesss: \t{}".format(song_data['song_hotttnesss'])
+    print "   Song currency:       {}".format(song_data['song_currency'])
+    print "   Song hotttnesss:     {}".format(song_data['song_hotttnesss'])
     print
-    print "\tArtist familiarity: \t{}".format(song_data['artist_familiarity'])
-    print "\tArtist hotttnesss: \t{}".format(song_data['artist_hotttnesss'])
-    print "\tArtist discovery: \t{}".format(song_data['artist_discovery'])
+    print "   Artist familiarity:  {}".format(song_data['artist_familiarity'])
+    print "   Artist hotttnesss:   {}".format(song_data['artist_hotttnesss'])
+    print "   Artist discovery:    {}".format(song_data['artist_discovery'])
 
 def en_info(catalog):
     """Display echnonest tasteprofile info"""
-    logger.debug('call en_info')
+    logger.debug('call function en_info')
     print
-    print "\tTotal songs/resolved: {} / {}".format(catalog['total'], catalog['resolved'])
-    print "\tID: {}".format(catalog['id'])
-    print "\tDate created: {}".format(catalog['created'])
-    print "\tPending tickets: {}".format(" / ".join(
+    print "   Total songs/resolved: {} / {}".format(catalog['total'], catalog['resolved'])
+    print "   ID:                   {}".format(catalog['id'])
+    print "   Date created:         {}".format(catalog['created'])
+    print "   Pending tickets:      {}".format(" / ".join(
         [pending_ticket['ticket_id'] for pending_ticket in catalog['pending_tickets']])
     )
 
 def en_sure_delete_tasteprofile(api_key, profile_id):
     """Warning before taste profile deletion."""
+    logger.debug('call function sure_delete_tasteprofile')
     print
     print "WARNING: you are about to delete your taste profile. All your"
     print "favorite, ban and skip data will be lost. playcount and rating"
@@ -263,5 +285,6 @@ def en_sure_delete_tasteprofile(api_key, profile_id):
 
 def en_delete():
     """Print delete output"""
+    logger.debug('call function en_delete')
     print
-    print "Your echonest profile has been deleted."
+    print "   Your echonest profile has been deleted."
